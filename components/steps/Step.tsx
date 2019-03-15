879dcc87c94af0341c796d2abbdf720e0046b7b5
yuanye <656480071@qq.com>
2019-03-15 10:20:03 +0800

    优化步骤条标题背景色

--- components/steps/Step.tsx
+++ components/steps/Step.tsx
@@ -24,6 +24,26 @@ export default class Step extends React.Component<StepProps> {
     stepStatus: 'wait',
     total: 0
   }
+  state: any
+
+  constructor(props: StepProps) {
+    super(props)
+
+    this.state= {
+      left: props.direction === 'h' ? 32 : 15
+    }
+  }
+
+  componentDidMount() {
+    const { labelPlacement, direction } = this.props
+    if (labelPlacement === 'h' && direction === 'h') {
+      const w = (this.refs.title as Element).clientWidth
+      const left = w + 32 + 2
+      this.setState({
+        left
+      })
+    }
+  }
 
   getIconComp(icon: string = '', tempStatus: string = 'wait', stepNumber: number = 1) {
     let iconComp
@@ -69,13 +89,13 @@ export default class Step extends React.Component<StepProps> {
     return (
       <View config={{...rest, prefix: this.prefix, cls, sty}}>
         <div className={headCls}>
-          <div className={`${this.prefix}-line`}>
+          <div className={`${this.prefix}-line`} style={{ left: `${this.state.left}px` }}>
             <span className={lineCls} />
           </div>
           <span className={`${this.prefix}-icon`}>{iconComp}</span>
         </div>
         <div className={descCls}>
-          <div className={`${this.prefix}-title`}>{title}</div>
+          <div ref='title' className={`${this.prefix}-title`}>{title}</div>
           <div className={`${this.prefix}-desc`}>{desc}</div>
         </div>
       </View>
