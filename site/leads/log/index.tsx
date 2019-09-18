139cc8888ff097924debc01d0dfb06e93e53faba
yuanye <656480071@qq.com>
2019-09-18 15:21:16 +0800

    logs

--- site/leads/log/index.tsx
+++ site/leads/log/index.tsx
@@ -14,7 +14,8 @@ export default class Log extends React.Component<LogProps> {
   render() {
     document.title = this.title
     const data = [
-      { time: '2019-09-12', title: '1.0.1版本正式发布', message: '涵盖63个组件' },
+      { time: '2019-09-12', title: '1.0.1版本正式发布', message: '包含63个ui组件' },
+      { time: '2019-09-18', title: '1.0.2版本正式发布', message: (<div>1、Moment增加对格林威治时间的支持<br />2、表单域通过state改变默认值<br />3、select和notification样式微调</div>) },
     ]
     return (
       <div className={`${ROOT_PREFIX}-lead-wrapper`}>
